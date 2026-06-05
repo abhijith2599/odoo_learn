90a93a54c22f493e2f7c8ce21ae88bae9e6875c8
Abhijith <abhijith.d@pearlsofttechnologies.com>
2026-06-05 18:42:08 +0530

    School class view added

---
+++ school_management/models/school_class.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class SchoolClass(models.Model):
+    _name = 'school.class'
+    _description = 'School Class'
+
+    name = fields.Char(string='Class Name', required=True)
+    student_ids = fields.One2many('school.student', 'class_id', string='Students')
+    student_count = fields.Integer(string='Student Count', compute='_compute_student_count')
+
+    @api.depends('student_ids')
+    def _compute_student_count(self):
+        for record in self:
+            record.student_count = len(record.student_ids)
+
+    def action_view_students(self):
+        self.ensure_one()
+        return {
+            'name': 'Students',
+            'type': 'ir.actions.act_window',
+            'res_model': 'school.student',
+            'view_mode': 'list,kanban,form',
+            'domain': [('class_id', '=', self.id)],
+            'context': {'default_class_id': self.id},
+            'target': 'current',
+        }
